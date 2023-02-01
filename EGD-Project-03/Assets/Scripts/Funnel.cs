38552763f6a8433d9d326cf8f13624986679e408
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2023-02-01 07:20:42 -0500

    Removed unneeded grinder code

--- EGD-Project-03/Assets/Scripts/Funnel.cs
+++ EGD-Project-03/Assets/Scripts/Funnel.cs
@@ -32,27 +32,5 @@ public class Funnel : MonoBehaviour
         }
 
         favorSound.GetComponent<AudioSource>().Play();
-
-        Vector2 pos = this.transform.position;
-        Vector2 cPos = collision.transform.position;
-        if (pos.x - colliderX <= cPos.x && pos.x + colliderX >= cPos.x)
-        {
-            SpriteRenderer sr = collision.GetComponent<SpriteRenderer>();
-            if (sr != null)
-            {
-                if (collision.gameObject.layer == LayerMask.NameToLayer("Sheep") &&
-                    pos.y <= cPos.y && collision.GetComponent<Rigidbody2D>().velocity.y < 0)
-                {
-                    sr.sortingOrder = -2;
-                    collision.gameObject.layer = LayerMask.NameToLayer("Dead Sheep");
-                }
-                if (collision.gameObject.layer == LayerMask.NameToLayer("Dead Sheep") &&
-                    pos.y > cPos.y && collision.GetComponent<Rigidbody2D>().velocity.y > 0)
-                {
-                    sr.sortingOrder = 0;
-                    collision.gameObject.layer = LayerMask.NameToLayer("Sheep");
-                }
-            }
-        }
     }
 }
